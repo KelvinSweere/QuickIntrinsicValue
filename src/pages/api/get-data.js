39b2dc62be6db823ee943a61a0ed2bab308e921d
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-08-22 09:14:48 +0200

    Checkout to fix bug from main.

--- src/pages/api/get-data.js
+++ src/pages/api/get-data.js
@@ -10,19 +10,37 @@ export default async (req, res) => {
         'defaultKeyStatistics',
         'earningsTrend',
         'summaryDetail',
+        'financialData',
       ],
     });
-    const pricePerShare = result.price.regularMarketPrice;
-    const earningsPerShare = result.defaultKeyStatistics.trailingEps;
+
+    const freeCashFlow = result?.financialData?.freeCashflow;
     const growthRate = result.earningsTrend.trend?.find(
       (x) => x.period === '+5y'
     )?.growth;
+    const pricePerShare = result.price.regularMarketPrice;
+    const earningsPerShare = result.defaultKeyStatistics.trailingEps;
     const currentYieldOfBond = 2.57;
     const currencySymbol = result.price.currencySymbol;
     const dividendYield = result.summaryDetail.dividendYield ?? 0;
     const peRation = result.summaryDetail.trailingPE;
 
     const isInvalid = growthRate === null || peRation === null;
+
+    const equity = result?.price?.marketCap;
+    const debt = result?.financialData?.totalDebt;
+
+    const costOfEquity = 0.1;
+    const costOfDebt = 0.05;
+    const taxRate = 0.3;
+    const totalCapital = equity + debt;
+    const wacc =
+      (equity / totalCapital) * costOfEquity +
+      (debt / totalCapital) * costOfDebt * (1 - taxRate);
+
+    console.log('freeCashFlow', freeCashFlow);
+    console.log('growthRate', growthRate);
+    console.log('wacc', wacc);
 
     return res.status(200).json({
       pricePerShare,
